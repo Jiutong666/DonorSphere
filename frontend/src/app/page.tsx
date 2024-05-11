1e056b9ff9b4dd30171bd67cf1abb7ad2d6a9fee
ShawBit <shawbitx@gmail.com>
2024-05-12 07:36:04 +0800

    feat: add the rainbowkit to connect wallets

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <ConnectButton />
+    </div>
+  );
 }
---
+++ frontend/src/components/Provider.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { config } from '@/configs/rainbowkit';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+
+const queryClient = new QueryClient();
+
+export default function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider modalSize="compact">{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
