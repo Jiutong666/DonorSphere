d0769702a2c1ee55def9ea03147bd07cf658b37e
NihilityHsiao <programmerHsiao@outlook.com>
2024-05-25 15:41:41 +0800

    🧪 test(VotingBaseTest): 增加测试样例
    1.修改testProposalCreation，targetAmount单位使用USD
    
    2.增加testDonate_NotEnoughUSD，测试 捐款额度小于5USD
    
    3.增加testDonate_EnoughUSD，测试捐款
    
    4.增加testDonate_NonExistCampaign，测试捐款给不存在的活动
    
    5.增加testDonate_NoInPeriod，在非捐款时间捐款
    
    6.增加testDonate_ReachTargetAmount，达到捐款额度后无法捐款
    
    7.增加testTransferDonations_NoAmountToBeSend，没有捐款时无法转账
    
    8.增加testTransferDonations_DonationHasBeenWithdrawn，测试重复转账

--- test/VotingBaseTest.t.sol
+++ test/VotingBaseTest.t.sol
@@ -15,7 +15,14 @@ contract VotingBaseTest is Test {
     address member3 = address(4);
     address dataFeedAddress = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419; // Mock data feed address
     address public nonMember = address(6);
+    address public beneficiary = address(7);
     PriceConverter price;
+
+    uint256 ONE_USD;
+    uint256 FIVE_USD;
+    uint256 TEN_USD;
+    uint256 constant MIN_DONATION_FIVE_USD = 5;
+    uint256 constant TARGET_DONATION_USD = 50; // 50 usd
 
     function setUp() public {
         votingToken = new VotingToken();
@@ -36,6 +43,48 @@ contract VotingBaseTest is Test {
         votingBase.addMember(member2);
         votingBase.addMember(member3);
         vm.stopPrank();
+
+        ONE_USD = price.USD(1);
+        FIVE_USD = price.USD(5);
+        TEN_USD = price.USD(10);
+
+        uint256 initialBalance = 10 ether;
+        vm.deal(member1, initialBalance);
+        vm.deal(member2, initialBalance);
+        vm.deal(member3, initialBalance);
+    }
+
+    modifier createProposalAndPassed() {
+        // 成员1创建提案
+        vm.startPrank(member1);
+        uint256 proposalId = votingBase.createProposal(
+            "Improve Voting System",
+            TARGET_DONATION_USD, //targetAmount(USD)
+            1653897600, //beginTime 2022-05-30 16:00:00
+            16999999999, //endTime 2508-09-16 14:13:19
+            7 days, //duration
+            beneficiary, // beneficiary
+            MIN_DONATION_FIVE_USD
+        );
+        vm.stopPrank();
+
+        // 成员投票
+        vm.startPrank(member1);
+        votingBase.vote(proposalId, true); // member1 投支持票
+        vm.stopPrank();
+
+        vm.startPrank(member2);
+        votingBase.vote(proposalId, true); // member2 投支持票
+        vm.stopPrank();
+
+        // 模拟时间流逝
+        vm.warp(block.timestamp + 8 days);
+
+        // 检查投票结果
+        vm.startPrank(member1);
+        votingBase.checkProposal(proposalId);
+        vm.stopPrank();
+        _;
     }
 
     /**
@@ -83,12 +132,12 @@ contract VotingBaseTest is Test {
         vm.startPrank(member1);
         uint256 id = votingBase.createProposal(
             "Improve Voting System",
-            100, //targetAmount
+            100, //targetAmount(USD)
             1653897600, //beginTime
             16999999999, //endTime
             7 days, //duration
             address(5),
-            50
+            MIN_DONATION_FIVE_USD
         );
         vm.stopPrank();
 
@@ -96,7 +145,7 @@ contract VotingBaseTest is Test {
         DataType.CampaignInfo memory proposal = votingBase.getProposal(id);
         assertEq(proposal.name, "Improve Voting System");
         assertEq(proposal.creator, member1);
-        assertEq(proposal.targetAmount, 100);
+        assertEq(proposal.targetAmount, price.USD(100));
         assertEq(proposal.beginTime, 1653897600);
         assertEq(proposal.endTime, 16999999999);
         assertEq(proposal.voteCount, 0);
@@ -357,5 +406,270 @@ contract VotingBaseTest is Test {
 
         vm.expectRevert("Voting period has not ended yet");
         votingBase.checkPass(id);
+    }
+
+    function testDonate_NotEnoughUSD() public createProposalAndPassed {
+        uint256 id = 1;
+
+        vm.prank(member1);
+        vm.expectRevert("you need to send more ETH");
+        votingBase.donate{value: ONE_USD}(id);
+
+        vm.prank(member2);
+        vm.expectRevert("you need to send more ETH");
+        votingBase.donate{value: 2 * ONE_USD}(id);
+
+        vm.prank(member3);
+        vm.expectRevert("you need to send more ETH");
+        votingBase.donate{value: FIVE_USD - 1}(id);
+    }
+
+    function testDonate_EnoughUSD() public createProposalAndPassed {
+        uint256 id = 1;
+        uint256 totalDonation = 0;
+
+        assertEq(address(votingBase).balance, 0);
+        {
+            // member1 捐款 5USD
+            uint256 beginBalance1 = member1.balance;
+            vm.prank(member1);
+            votingBase.donate{value: FIVE_USD}(id);
+            uint256 endBalanace1 = member1.balance;
+            totalDonation += FIVE_USD;
+
+            assertEq(beginBalance1 - endBalanace1, FIVE_USD);
+            assertEq(address(votingBase).balance, totalDonation);
+        }
+
+        // member2 捐款10USD
+        {
+            uint256 beginBalance2 = member2.balance;
+            vm.prank(member2);
+            votingBase.donate{value: TEN_USD}(id);
+            uint256 endBalance2 = member2.balance;
+            totalDonation += TEN_USD;
+            assertEq(beginBalance2 - endBalance2, TEN_USD);
+            assertEq(address(votingBase).balance, totalDonation);
+        }
+
+        // member3 捐款10USD
+        {
+            uint256 beginBalance3 = member3.balance;
+            vm.prank(member3);
+            votingBase.donate{value: TEN_USD}(id);
+            uint256 endBalance3 = member3.balance;
+            totalDonation += TEN_USD;
+            assertEq(beginBalance3 - endBalance3, TEN_USD);
+            assertEq(address(votingBase).balance, totalDonation);
+        }
+
+        // 总共收到25USD
+        assertEq(address(votingBase).balance, FIVE_USD + TEN_USD + TEN_USD);
+        assertEq(address(votingBase).balance, totalDonation);
+    }
+
+    function testDonate_NonExistCampaign() public createProposalAndPassed {
+        // createProposalAndPassed 会添加一个campaign，id为1
+        vm.startPrank(member1);
+        vm.expectRevert("campaign not exist");
+        votingBase.donate{value: FIVE_USD}(2);
+        vm.stopPrank();
+
+        vm.startPrank(member2);
+        vm.expectRevert("campaign not exist");
+        votingBase.donate{value: FIVE_USD}(3);
+        vm.stopPrank();
+
+        vm.startPrank(member3);
+        vm.expectRevert("campaign not exist");
+        votingBase.donate{value: FIVE_USD}(440);
+        vm.stopPrank();
+    }
+
+    function testDonate_NoInPeriod() public {
+        uint256 votingDuration = 1 days;
+        uint256 beginTime = block.timestamp + 7 days;
+        uint256 endTime = beginTime + 7 days;
+        uint256 proposalId = 0;
+        // 创建并通过提案
+        {
+            // 成员1创建提案
+            vm.startPrank(member1);
+            proposalId = votingBase.createProposal(
+                "Improve Voting System",
+                100, //targetAmount(USD)
+                beginTime, // 当前时间戳的 7 天后
+                endTime, // beginTime的7天后
+                votingDuration, //1天
+                address(6), // beneficiary
+                MIN_DONATION_FIVE_USD
+            );
+            vm.stopPrank();
+
+            // 成员投票
+            vm.startPrank(member1);
+            votingBase.vote(proposalId, true); // member1 投支持票
+            vm.stopPrank();
+
+            vm.startPrank(member2);
+            votingBase.vote(proposalId, true); // member2 投支持票
+            vm.stopPrank();
+
+            // 模拟时间流逝
+            vm.warp(block.timestamp + votingDuration);
+
+            // 检查投票结果
+            vm.startPrank(member1);
+            votingBase.checkProposal(proposalId);
+            vm.stopPrank();
+        }
+
+        // 捐款活动还没开始:campaign not start
+        {
+            vm.startPrank(member1);
+            vm.expectRevert("campaign not start");
+            votingBase.donate{value: FIVE_USD}(proposalId);
+            vm.stopPrank();
+        }
+
+        // 捐款活动结束了:campaign has ended
+        {
+            // 模拟时间流逝
+            vm.warp(endTime + 1 days);
+            vm.startPrank(member1);
+            vm.expectRevert("campaign has ended");
+            votingBase.donate{value: FIVE_USD}(proposalId);
+            vm.stopPrank();
+        }
+    }
+
+    function testDonate_ReachTargetAmount() public createProposalAndPassed {
+        uint256 proposalId = 1;
+
+        // member1 捐款 TARGET_DONATION_USD - 1 USD的金额
+        // 距离目标金额还差1USD
+        uint256 donateAmount = price.USD(TARGET_DONATION_USD) - ONE_USD; //  距离目标金额还差1USD
+        vm.startPrank(member1);
+        votingBase.donate{value: donateAmount}(proposalId);
+        vm.stopPrank();
+
+        // member2 捐款 5 USD
+        vm.startPrank(member2);
+        votingBase.donate{value: FIVE_USD}(proposalId);
+        vm.stopPrank();
+
+        // member3 捐款 5USD
+        vm.startPrank(member3);
+        vm.expectRevert("reached target amount");
+        votingBase.donate{value: FIVE_USD}(proposalId);
+        vm.stopPrank();
+    }
+
+    function createCampaign(
+        address beneficiaryAddr,
+        uint256 targetUSD,
+        uint256 minDonationUSD,
+        uint256 beginTime,
+        uint256 endTime
+    ) private returns (uint256) {
+        // 成员1创建提案
+        vm.startPrank(member1);
+        uint256 proposalId = votingBase.createProposal(
+            "Improve Voting System",
+            targetUSD, //targetAmount(USD)
+            beginTime, //beginTime 2022-05-30 16:00:00
+            endTime, //endTime 2508-09-16 14:13:19
+            1 days, //duration
+            beneficiaryAddr, // beneficiary
+            minDonationUSD
+        );
+        vm.stopPrank();
+
+        // 成员投票
+        vm.startPrank(member1);
+        votingBase.vote(proposalId, true); // member1 投支持票
+        vm.stopPrank();
+
+        vm.startPrank(member2);
+        votingBase.vote(proposalId, true); // member2 投支持票
+        vm.stopPrank();
+
+        // 模拟时间流逝
+        vm.warp(block.timestamp + 1 days);
+
+        // 检查投票结果
+        vm.startPrank(member1);
+        votingBase.checkProposal(proposalId);
+        vm.stopPrank();
+
+        return proposalId;
+    }
+
+    function testTransferDonations_NoAmountToBeSend() public {
+        uint256 targetUSD = 100; // 100 USD
+        uint256 minDonationUSD = 5;
+        uint256 beginTime = block.timestamp + 2 days;
+        uint256 endTime = beginTime + 7 days;
+        uint256 proposalId = createCampaign(
+            beneficiary,
+            targetUSD,
+            minDonationUSD,
+            beginTime,
+            endTime
+        );
+
+        // 模拟时间流逝，没人捐款
+        vm.warp(endTime + 1);
+
+        // 转账
+        vm.startPrank(owner);
+        vm.expectRevert("no amount to be send");
+        votingBase.transferDonations(proposalId);
+        vm.stopPrank();
+    }
+
+    // 测试重复转账
+    function testTransferDonations_DonationHasBeenWithdrawn() public {
+        uint256 targetUSD = 100; // 100 USD
+        uint256 minDonationUSD = 5;
+        uint256 beginTime = block.timestamp + 2 days;
+        uint256 endTime = beginTime + 7 days;
+        uint256 proposalId = createCampaign(
+            beneficiary,
+            targetUSD,
+            minDonationUSD,
+            beginTime,
+            endTime
+        );
+
+        vm.warp(beginTime);
+        // member1 捐款200USD
+        uint256 TWO_HUNDRED_USD = TEN_USD * 20;
+        vm.startPrank(member1);
+        votingBase.donate{value: TWO_HUNDRED_USD}(proposalId);
+        vm.stopPrank();
+        // 模拟时间流逝，捐款活动结束
+        vm.warp(endTime + 1);
+
+        uint256 beginVotingBaseBalance = address(votingBase).balance;
+        uint256 beginBeneficiaryBalance = beneficiary.balance;
+
+        // 转账
+        vm.startPrank(owner);
+        votingBase.transferDonations(proposalId);
+        vm.stopPrank();
+
+        uint256 endVotingBaseBalance = address(votingBase).balance;
+        uint256 endBeneficiaryBalance = beneficiary.balance;
+        assertEq(endVotingBaseBalance, 0);
+        assertEq(
+            beginVotingBaseBalance + beginBeneficiaryBalance,
+            endBeneficiaryBalance
+        );
+
+        vm.startPrank(owner);
+        vm.expectRevert("donation has been withdrawn");
+        votingBase.transferDonations(proposalId);
+        vm.stopPrank();
     }
 }
