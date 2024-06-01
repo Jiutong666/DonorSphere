c93c169c3ec8e9e46c8b86da6fdd73888bdf96c7
ShawBit <shawbitx@gmail.com>
2024-06-01 09:03:09 +0800

    feat: add the donate with chain

--- frontend/src/components/DonateForm.tsx
+++ frontend/src/components/DonateForm.tsx
@@ -1,41 +1,51 @@
 'use client';
+import { VotingBase } from '@/abis/VotingBase';
+import { VotingBaseAddress } from '@/constants';
 import { Avatar, Button, Image, Input } from '@nextui-org/react';
 import { useSearchParams } from 'next/navigation';
-import { ChangeEvent, useState } from 'react';
+import { parseEther } from 'viem';
+import { useWriteContract } from 'wagmi';
 
 export default function DonateForm() {
   const searchParams = useSearchParams();
   const id = searchParams.get('projectId');
   const projectTitle = searchParams.get('projectTitle');
 
-  const [amount, setAmount] = useState('');
-  const amountHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    // 使用正则表达式匹配数字和小数点
-    const regex = /^\d*\.?\d*$/;
-    if (regex.test(inputValue)) {
-      setAmount(inputValue);
-    }
+  const { writeContractAsync } = useWriteContract();
+
+  const submitHandler = (formDate: FormData) => {
+    const amount = formDate.get('amount');
+    writeContractAsync({
+      abi: VotingBase,
+      address: VotingBaseAddress,
+      functionName: 'donate',
+      args: [BigInt(id as string)],
+      value: parseEther(amount as string),
+    });
   };
 
   return (
     <div className="flex justify-around gap-8">
-      <form className="p-6 bg-white rounded-lg border-1 w-[50%] flex flex-col gap-6">
+      <form className="p-6 bg-white rounded-lg border-1 w-[50%] flex flex-col gap-6" action={submitHandler}>
         <div className="rounded-xl border-1 p-4 bg-white">
           <p className="font-semibold text-[1.2rem] mb-4">100% goes to the project always.</p> Every donation is
           peer-to-peer, with no fees and no middlemen.
         </div>
-        <Input label="Address" value="0000" labelPlacement="outside" isDisabled isRequired />
         <Input
-          value={amount}
-          placeholder="Amount"
+          type="number"
+          name="amount"
           label="Amount"
           labelPlacement="outside"
+          placeholder="Please enter amount"
           isRequired
-          onChange={amountHandler}
+          endContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">ETH</span>
+            </div>
+          }
         />
         <Button type="submit" className="font-bold text-[1rem]" color="primary">
-          Transfer
+          Donate
         </Button>
       </form>
       <div className="w-[50%] border-1 rounded-lg p-4 flex flex-col gap-6">
@@ -48,17 +58,17 @@ export default function DonateForm() {
         />
         <div className=" bg-white rounded-xl flex flex-col gap-4">
           <p className="font-semibold text-[1.5rem] overflow-hidden overflow-ellipsis whitespace-nowrap">
-            {projectTitle}111
+            {projectTitle}
           </p>
           <div className="flex gap-4 items-center ">
             <Avatar src="" />
-            <p className="text-[--main-color] font-medium">{}Shawbit</p>
+            <p className="text-[--main-color] font-medium">{}</p>
           </div>
           <div className="flex gap-4 items-center">
             <p className="text-gray-500 text-[1.2rem]"> Raised:</p>
             <p className="text-green-500 text-[1.5rem] font-normal">${`0`}</p>
           </div>
-          <p className=" text-[#656c82] h-[4.5rem] overflow-hidden">{}description</p>
+          <p className=" text-[#656c82] h-[4.5rem] overflow-hidden">{}</p>
         </div>
       </div>
     </div>
