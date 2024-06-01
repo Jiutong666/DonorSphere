1e36ef97b8380de9507da55f22f47b7e8b8c79f8
ShawBit <shawbitx@gmail.com>
2024-06-01 09:05:09 +0800

    feat: add the create project

--- frontend/src/components/ProjectForm.tsx
+++ frontend/src/components/ProjectForm.tsx
@@ -1,10 +1,36 @@
 'use client';
+import { VotingBase } from '@/abis/VotingBase';
 import { ImageUploadPreview } from '@/components/ImageUploadPreview';
+import { VotingBaseAddress } from '@/constants';
 import { Button, DateInput, Input, Textarea } from '@nextui-org/react';
 import { useState } from 'react';
+import { useWriteContract } from 'wagmi';
 
 export default function ProjectForm() {
-  const submitHandler = (formData: FormData) => {};
+  const { writeContractAsync } = useWriteContract();
+  const submitHandler = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    const targetAmount = formData.get('targetAmount');
+    const beneficiary = formData.get('beneficiary') as `0x${string}`;
+    const startDate = formData.get('startDate');
+    const endDate = formData.get('endDate');
+    const duration = formData.get('duration');
+
+    writeContractAsync({
+      abi: VotingBase,
+      address: VotingBaseAddress,
+      functionName: 'createProposal',
+      args: [
+        title,
+        BigInt(targetAmount as number),
+        BigInt(new Date(startDate as string).getTime()),
+        BigInt(new Date(endDate as string).getTime()),
+        BigInt(duration as number),
+        beneficiary,
+        BigInt(0),
+      ],
+    });
+  };
   const [imagePreview, setImagePreview] = useState('');
 
   return (
@@ -43,7 +69,7 @@ export default function ProjectForm() {
       />
       <Input
         isRequired
-        type="text"
+        type="number"
         label="Target Amount"
         name="targetAmount"
         labelPlacement="outside"
@@ -51,7 +77,16 @@ export default function ProjectForm() {
         placeholder="Please enter your target amount"
       />
 
+      <DateInput label="Start Date" labelPlacement="outside" name="startDate" isRequired />
       <DateInput label="End Date" labelPlacement="outside" name="endDate" isRequired />
+      <Input
+        type="number"
+        label="Duration"
+        labelPlacement="outside"
+        name="duration"
+        placeholder="Please enter the duration days"
+        isRequired
+      />
 
       <Button type="submit" className="w-[11rem] bg-[--main-color] text-white font-bold mx-auto">
         Submit
