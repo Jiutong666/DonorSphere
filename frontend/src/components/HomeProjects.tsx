0fa0db99d5d87823119d115af507f449b6198bbc
ShawBit <shawbitx@gmail.com>
2024-06-02 11:17:05 +0800

    feat: complete the project's card

--- frontend/src/components/HomeProjects.tsx
+++ frontend/src/components/HomeProjects.tsx
@@ -3,16 +3,14 @@ import { VotingBase } from '@/abis/VotingBase';
 import { config } from '@/configs/rainbowkit';
 import { VotingBaseAddress } from '@/constants';
 import { Proposal } from '@/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useReadContract } from 'wagmi';
 import { readContract } from 'wagmi/actions';
 import ProjectCardItem from './ProjectCardItem';
 import VoteCardItem from './VoteCardItem';
 
 export default function HomeProjects() {
-  const projects: Proposal[] = [];
-
-  const [projectsLength, setProjectsLength] = useState(projects.length);
+  const [projects, setProjects] = useState<Proposal[]>([]);
 
   const ids = useReadContract({
     abi: VotingBase,
@@ -20,58 +18,60 @@ export default function HomeProjects() {
     functionName: 'getAllProposalIds',
   });
 
-  if (ids.data && ids.isFetched && !projectsLength) {
-    ids.data.forEach((id: bigint) => {
-      readContract(config, {
+  const getProjects = async (ids: any) => {
+    const requests = ids.map((id: any) => {
+      return readContract(config, {
         abi: VotingBase,
         address: VotingBaseAddress,
         functionName: 'getProposal',
         args: [id],
-      }).then((value: Proposal) => {
-        projects.push(value);
-        setProjectsLength(projects.length);
       });
     });
-  }
 
-  if (!projectsLength) {
-    return <div>Hold on</div>;
-  }
+    Promise.all(requests).then((results) => setProjects(results));
+  };
+
+  useEffect(() => {
+    if (ids.data && ids.isFetched && ids.data.length) {
+      getProjects(ids.data as bigint[]);
+    }
+  }, [ids.data, ids.isFetched]);
 
   return (
     <div className="max-w-[114rem] w-full mt-20 bg-white px-[20rem] py-10">
-      {!projectsLength ? (
+      {!projects ? (
         <div>Please hold on ...</div>
       ) : (
         <div>
           <div className="">
             <p className="font-bold text-[3rem] text-[#515768] mb-4">Projects</p>
-            <div className="flex flex-row flex-wrap gap-9 justify-between">
-              {projects.map((project) => {
-                project.passed ? (
-                  <ProjectCardItem
-                    key={project.name}
-                    id={project.id}
-                    name={project.name}
-                    creator={'0xAA'}
-                    length={projectsLength}
-                    currentAmount={project.currentAmount}
-                    targetAmount={project.targetAmount}
-                    beneficiary={project.beneficiary}
-                  />
+            <div className="flex flex-row flex-wrap gap-9 justify-start">
+              {projects.map((project, index) =>
+                project.id !== BigInt(0) ? (
+                  project?.passed ? (
+                    <ProjectCardItem
+                      key={index}
+                      id={project?.id}
+                      name={project?.name}
+                      creator={'0xAA'}
+                      currentAmount={project?.currentAmount}
+                      targetAmount={project?.targetAmount}
+                      beneficiary={project?.beneficiary}
+                    />
+                  ) : (
+                    <VoteCardItem
+                      key={index}
+                      id={project?.id}
+                      name={project?.name}
+                      creator={'0xAA'}
+                      targetAmount={project?.targetAmount}
+                      beneficiary={project?.beneficiary}
+                    />
+                  )
                 ) : (
-                  <VoteCardItem
-                    key={project.name}
-                    id={project.id}
-                    name={project.name}
-                    creator={'0xAA'}
-                    length={projectsLength}
-                    currentAmount={project.currentAmount}
-                    targetAmount={project.targetAmount}
-                    beneficiary={project.beneficiary}
-                  />
-                );
-              })}
+                  ''
+                )
+              )}
             </div>
           </div>
         </div>
